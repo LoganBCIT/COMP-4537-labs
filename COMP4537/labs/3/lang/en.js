758fc634359d1a1565c7758ff9c96f0834802d2c
Logan Dutton-Anderson <logananderson4549@gmail.com>
2025-09-25 15:57:48 -0700

    Fix: CommonJS modules + add notFound + vercel.json

--- COMP4537/labs/3/lang/en.js
+++ COMP4537/labs/3/lang/en.js
@@ -1,5 +1,5 @@
 // lang/en.js
-export default {
+module.exports = {
   qName: "name",
   msg: "Hello {name}, What a beautiful day. Server current date and time is ",
   hContentType: "Content-Type",
@@ -8,5 +8,6 @@ export default {
   htmlEnd: "</body></html>",
   blueOpen: "<p style=\"color: blue;\">",
   blueClose: "</p>",
-  errMissing: "Missing required query parameter: name"
+  errMissing: "Missing required query parameter: name",
+  notFound: "Not Found"
 };
